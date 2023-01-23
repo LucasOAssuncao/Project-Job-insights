8d079c315260baf85d45920d4b3aa29ad74768bf
Lucas Otoni <lotoni.vivo@gmail.com>
2023-01-23 15:21:36 -0300

    get_unique_job_types implementada

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -15,20 +15,9 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
+    data = read(path)
+    job_types = [job["job_type"] for job in data]
+    return list(set(job_types))
     raise NotImplementedError
 
 
