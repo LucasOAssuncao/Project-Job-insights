3a0991a86ec017d834d3cb3925bebbc94634ffa6
Lucas Otoni <lotoni.vivo@gmail.com>
2023-01-23 16:32:11 -0300

    matches_salary_range implementada

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -21,28 +21,15 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
+    if "min_salary" not in job or "max_salary" not in job:
+        raise ValueError("min_salary or max_salary missing")
+    if not (isinstance(job["min_salary"], (int, float)) and
+            isinstance(job["max_salary"], (int, float))):
+        raise ValueError("min_salary or max_salary is not a number")
+    if job["min_salary"] > job["max_salary"]:
+        raise ValueError("min_salary is greater than max_salary")
+    print(job["min_salary"], job["max_salary"], 'OIOIOI')
+    return job["min_salary"] <= int(salary) <= job["max_salary"]
     raise NotImplementedError
 
 
