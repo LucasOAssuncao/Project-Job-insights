574e05a24e05b6b17d1613f80bbe58322a96d50a
Lucas Otoni <lotoni.vivo@gmail.com>
2023-01-24 10:13:36 -0300

    test: sort_by implementado

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,25 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+
+mock = [
+    {"min_salary": 0, "max_salary": 20},
+    {"min_salary": 100, "max_salary": 200},
+]
+
+order_min_salary = [
+    {"min_salary": 0, "max_salary": 20},
+    {"min_salary": 100, "max_salary": 200},
+]
+
+order_max_salary = [
+    {"min_salary": 100, "max_salary": 200},
+    {"min_salary": 0, "max_salary": 20},
+]
+
+
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(mock, "max_salary")
+    assert mock == order_max_salary
+    sort_by(mock, "min_salary")
+    assert mock == order_min_salary
