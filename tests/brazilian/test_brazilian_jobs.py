831cfdd52cb7c31279a27648fa1ca3cba9190707
Lucas Otoni <lotoni.vivo@gmail.com>
2023-01-24 10:05:16 -0300

    test: read_brazilian_file implementado

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    mock = {"title": "Motorista", "salary": "3000", "type": "full time"}
+    brazilian_jobs = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    for job in brazilian_jobs:
+        print(mock.keys())
+        assert mock.keys() == job.keys()
